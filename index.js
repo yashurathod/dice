e668d9dbf3581339e3ed6c28a87a7371329743fe
Yashodhar S Rathod <42674631+yashurathod@users.noreply.github.com>
2021-05-01 20:56:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var randomNumber1=Math.round((Math.random()*5)+1);
+document.querySelector(".img1").setAttribute("src","images/dice" + randomNumber1 +".png");
+var randomNumber2=Math.round((Math.random()*5)+1);
+document.querySelector(".img2").setAttribute("src","images/dice" + randomNumber2 +".png");
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML="Player 1 wins";
+}
+else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").innerHTML="Player 2 wins";
+}
+else{
+  document.querySelector("h1").innerHTML="Draw!!";
+}
