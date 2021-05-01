393c0b1828f5fd6e5d2a3169db9909e24c406a39
Yashodhar S Rathod <42674631+yashurathod@users.noreply.github.com>
2021-05-01 21:16:57 +0530

    updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var randomNumber2=Math.round((Math.random()*5)+1);
 document.querySelector(".img2").setAttribute("src","images/dice" + randomNumber2 +".png");
 
 if(randomNumber1 > randomNumber2){
-  document.querySelector("h1").innerHTML="Rohit wins";
+  document.querySelector("h1").innerHTML="Rohit wins ";
 }
 else if(randomNumber1<randomNumber2){
   document.querySelector("h1").innerHTML="Yashodhar wins";
